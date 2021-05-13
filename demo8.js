49528515a84ab24289a3b8b7c45d730504d13055
fvgtekken <federicovongromann@gmail.com>
2021-05-13 19:35:21 -0300

    ejercisios js

---
+++ demo8.js
@@ -0,0 +1,12 @@
+function getNameGames(juegos){
+
+var juegos = {juegosjugado:'roblox',juegosAtual:'arsenal',juegosfuturo:'my island'}
+
+if(juegos==='myisland'){}
+
+console.log(juegos.juegosfuturo)
+
+return
+}
+getNameGames()
+var sibolo={numero:12,nombre:'fede', Boolean:true}
