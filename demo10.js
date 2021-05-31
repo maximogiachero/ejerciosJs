76100bddb7ad3f6e8f5de9651bc20546dd3ac21b
fvgtekken <federicovongromann@gmail.com>
2021-05-31 20:01:16 -0300

    ejersisios

---
+++ demo10.js
@@ -0,0 +1,28 @@
+// Crear si un array con el nombre  fps con tres elementos se acurdan como se hacia? [ elemento, elemento, elemeto],
+// el primer elemento se va a llamar frame
+// el segundo elemento se va llamr per
+// y el tercero seconds
+
+
+// Crear un Obj  que se llame roblox  con tres propiedades fps, personaje, escenario
+
+
+// crear una variable normal que contenga un string con el nombre name
+// crear otra variable normal que contengo un entero con el nombre number
+// cerar otro variable normal tipo boolen con el valor true
+
+
+
+
+
+
+
+var fps=['frames', 'per', 'second']
+
+var roblox={fps:'80', personaje:'biulderman',esenario:'disparos'}
+
+var name ='maxi'
+
+var number = 43
+
+var boolen = 'true'
---
+++ demo9.js
@@ -0,0 +1,30 @@
+var juegos=['batman','my island','arsenal']
+
+console.log(juegos[1])
+
+var chipote=[5,'valentino jaja',true]
+
+//console.log(chipote[2])
+
+//console.log(chipote[0])
+
+
+//console.log(chipote[1])
+
+
+
+var yanque=[
+
+ {name:'chipote chillon'},
+
+ 51,
+
+ true
+
+ ]
+
+console.log(yanque[0].name)
+
+console.log(yanque[1])
+
+console.log(yanque[2])
