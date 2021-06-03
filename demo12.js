b1eba790f4b77ac5d5b27abc215e8890e1381eb2
fvgtekken <federicovongromann@gmail.com>
2021-06-03 19:33:54 -0300

    mas ejercicios

---
+++ demo12.js
@@ -0,0 +1,38 @@
+  // Crear un array con dos elementos:
+  // Un elemento tipo string con el nombre "batman"
+  // Un elemento tipo int con el varlor 1
+  // imprimir los dos elementos con console log
+
+ 
+  // Crear un if que pregunte si el elemento numero 1 del array es iugal a batman
+  // Si es asi imprimir el en console log "Si es Batman!
+  
+
+  // Crear un objecto llamado pais con dos propiedades provincia1 y provincia2
+  // cada propiedad debe tener como valor un string con el nombre de uuna provincia de argentina
+  // imprimir alguna propiedad con console log
+
+
+
+  // Solo responder si es array, objeto, boolean, entero o string.
+  // cuando a una variable se le asinga esto {}, que se le esta asignando ? objeto
+  // cuando a una variable se le asinga esto [], que se le esta asignando ? aray
+  // Cuando a una variable se le asiga esto "Soy Chipote chipoton", que tipo de valor es? un string
+  // Cuando a una varialbe  se le asigna 89, que tipo de valor es? entero
+  // Cuando a una variable se la asingna el valor false o true, que tipo de valor es? boolean
+
+  var juegos =['batman', 1]
+
+  console.log(juegos[0])
+
+  console.log(juegos[1])
+
+  if(juegos[1]==='batman'){
+console.log('si es batman')
+
+  }
+
+  var pais={provincia1:'santa cruz',provincia2:'chubut'}
+
+
+console.log(pais.provincia1)
