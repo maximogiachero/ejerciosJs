e03488bab69f72fb920302ecefd0261ca5225c52
fvgtekken <federicovongromann@gmail.com>
2021-06-10 19:28:53 -0300

    mas ejercicios

---
+++ demo14.js
@@ -0,0 +1,27 @@
+
+
+  // imprimir el valor del elemento  0
+  // imprimir el valor del elemento  1
+  // imprimir el valor del elemento  2
+//var prueba=["batman", true, 1000]
+
+ //var suma= 123+prueba[2]
+
+//console.log(suma)
+//var prueba=["batman", 899, {monos:'baguino'} ]
+
+
+//console.log(prueba[0])
+
+//console.log(prueba[1])
+
+//console.log(prueba[2])
+
+//var objeto=prueba[2]
+
+//console.log(objeto.monos)
+
+var suma={suma1:10,suma2:30}
+
+var matematica=1888+suma.suma1+suma.suma2
+console.log(matematica)
