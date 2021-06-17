1632cea8a2e78ed4546d1b5ecba88cf7b66fbe34
fvgtekken <federicovongromann@gmail.com>
2021-06-17 19:49:08 -0300

    mas ejercicios

---
+++ demo15.js
@@ -0,0 +1,16 @@
+// Se crear un array que se llame juegos con 8 elementos, cada elemento del array debe ser un 
+// string con el nombre del juego que a uds les guste. 
+
+
+var chavoDel8=[8,'programa mexicano',{persona1:'chavo', persona2:'don ramon',persona3:'doña florinda'}]
+
+console.log(chavoDel8[1])
+
+console.log(chavoDel8[2].persona2)
+
+var juegos={juegos1:'batman', juegos2:'arsenal',juegos3:'jailbreak'}
+
+console.log(juegos.juegos2)
+
+var juego=['batman akham asilum','batman arkhama city','arsenal','my island','funki friday','jailbreak','bluid boat','tycoon','half life']
+
