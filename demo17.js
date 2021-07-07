dd299cd702cfea7610744fcbfc415bbbcbc0152d
fvgtekken <federicovongromann@gmail.com>
2021-07-07 19:27:48 -0300

    mas y mas ejercicios

---
+++ demo17.js
@@ -0,0 +1,42 @@
+///Hola chicos soy Federico el mas chipote chipoton aca les voy
+ // a pasar un ejercicio super yanque lleno de exito por que la verdad
+ // batamn y robin son muy pelotudos, mas pelotudo es robin y la verdad
+ // que a ese si le pegan las balas. Ademas no lo aguanto mas
+ // a la chipoteada por que es muy chipotaaa.
+ // cualquier cosa me avisan por que aca va el ejercicio:
+
+ // Crear un array con 4 elementos dos strings y dos enteros
+// cada elemento insertarlo con push
+// imprimir el array
+
+  var juegos=[]
+ function crearJuego(nombre){
+
+juegos.push(nombre)
+
+console.log(juegos)
+ } 
+ crearJuego(true)
+
+ crearJuego('bananaman')
+
+ crearJuego(868594956588)
+
+ crearJuego({nombre:'maxc3r 66'})
+
+
+ console.log(juegos[3])
+
+ var noSeQuePoner=['maxc3r 66 es el mejor','pero si es el mejor yutuber',546467847,849589375]
+
+ noSeQuePoner.push(noSeQuePoner)
+
+ console.log(noSeQuePoner[0])
+
+ 
+ console.log(noSeQuePoner[1])
+
+ console.log(noSeQuePoner[2])
+
+ 
+ console.log(noSeQuePoner[3])
