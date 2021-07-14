33f0dbb325cdbb091d86ac3d3a1bb65f0064c2cc
fvgtekken <federicovongromann@gmail.com>
2021-07-14 19:14:30 -0300

    mas y mas ejercisios

---
+++ demo18.js
@@ -0,0 +1,19 @@
+// crear un objeto simbolos que tenga tres propiedades, numero1, numero2 y verdadero
+ // a la propiedad numero1 asignarle el entero 12 y a la propiedad numero2 asignarle el numero entero 20
+ // a la propiedad verdadero asingarle un boolean
+
+ // sumar las propiedades de numero1 y dos e imprimirlas
+
+
+
+  var array=[true,'pumptrack',{juego1:'batman'}]
+
+  console.log(array[2])
+
+  var startWars={amigo:'rey',enemigo:'veyder',arma:'sable'}
+
+  console.log(startWars.arma,startWars.amigo)
+
+  var simbolos={numero1:12,numero2:20,verdadero:true}
+
+  console.log(simbolos.numero1+simbolos.numero2)
